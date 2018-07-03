437f7d7e2f027e9b6bb4860feefded32ced50afb
Courtenay <court.eccles@gmail.com>
2018-07-03 09:09:00 -0600

    added media query and other small changes

--- src/functions.test.js
+++ src/functions.test.js
@@ -3,3 +3,11 @@ const functions = require('./functions')
 test('Adds 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4)
 }) 
+
+test('Multiplies 2 * 2 to equal 4', () => {
+  expect(functions.multiply(2, 2)).toBe(4)
+})
+
+test('show me the module', () => {
+  functions.showMe()
+})
